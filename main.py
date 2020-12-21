71f78ec863f61759dffd3f18a5bf18d1c02cc244
Lana Chebib <lanachebib@Lanas-MacBook-Pro.local>
2020-12-20 17:28:40 -0800

    update code in users.py, implemented two tables

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from room_to_grow import User, Plant
+import users
+# set current_user to blank
+current_user = None
 
-username = input("Enter your username: ")
-name = input("Enter your name: ")
-zip = input("Enter your 5-digit Zip Code (US) or 6-digit Postal Code with no spaces (UK/Canada): ")
-currentUser = User(username,name,zip)
+welcome = input("Welcome! Are you a new user? (Y/N): ")
+
+if welcome = "N":
+  existing_user = input("Okay, you are an existing user. Enter your username: ")
+  print(f"Welcome back, {existing_user}")
+  current_user = existing_user
+else:
+  print("Let's create your account. ")
+  username = input("Enter a username: ")
+  name = input("Enter your name: ")
+  zip = input("Enter your 5-digit Zip Code (US) or 6-digit Postal Code with no spaces (UK/Canada): ")
+  new_user = User(username,name,zip)
+  current_user = new_user
 
 plant_name = input("Enter your new plant's name: ")
 plant_breed = input(f"Enter in {plant_name}'s breed: ")
-currentUser.create_plant(plant_name, plant_breed)
+current_user.create_plant(plant_name, plant_breed)
+response = input("Would you like to add any more plants? Y | N")
+if response == "Y":
+  plant_name = input("Enter your new plant's name: ")
+  plant_breed = input(f"Enter in {plant_name}'s breed: ")
+  current_user.create_plant(plant_name, plant_breed)
+elif reponse == "N":
+  print(f"Okay{new_user.username}, see ya!")
+else:
+  print("That is not a valid answer")
 
-print(username)
-print(name)
-print(zip)
-print(plant_name)
-print(plant_breed)
-print(currentUser)
+# print(username)
+# print(name)
+# print(zip)
+# print(plant_name)
+# print(plant_breed)
+# print(current_user.username)
+
+users.create_user_info()
+users.enter_user_info(current_user)
+
+
+users.create_plant_info()
+users.enter_plant_info(current_user.Plants[0])
--- users.py
+++ users.py
@@ -12,26 +12,48 @@ import sqlite3
 #each plant instance will have a property tied to the user so when we want to recall a certain plant we know which user it belongs to
 
 
+# c = conn.cursor()
+# c.execute("CREATE TABLE users (username TEXT);")
+
+
+# def plantCreator(name, breed):
+#   newPlant = Plant(name,breed )
+#   query = "INSERT INTO users SELECT  (?)"
+
+#   c.execute(query, name, breed)
+
+# def insertUser(user):
+#   # data = (name,zip, newPlant)
+#   query = "INSERT INTO users VALUES (?,)"
+#   c.execute(query,user)
+
+# conn.commit()
+# conn.close()
+
 conn = sqlite3.connect("users.db")
 
-c = conn.cursor()
+def create_user_info():
+  c = conn.cursor()
+  c.execute('CREATE TABLE IF NOT EXISTS user_info (username TEXT, name TEXT, zipcode BLOB)')
+  c.close()
 
-# c.execute("CREATE TABLE users (first_name TEXT, zip_code TEXT);")
+def enter_user_info(user):
+  c = conn.cursor()
+  data = (user.username, user.name, user.location)
+  query = "INSERT INTO user_info VALUES (?,?,?)"
+  c.execute(query, data)
+  conn.commit()
+  c.close()
 
-n = input("Please enter your name: ")
-z = input("Please enter your zip/area code: ")
+def create_plant_info():
+  c = conn.cursor()
+  c.execute('CREATE TABLE IF NOT EXISTS plant_info (name TEXT, breed TEXT, owner TEXT)')
+  c.close()
 
-def plantCreator(name, breed):
-  newPlant = Plant(name,breed )
-  query = "INSERT INTO users SELECT  (?)"
-
-  c.execute(query, name, breed)
-
-def insertUser(n,z):
-  # data = (name,zip, newPlant)
-  query = "INSERT INTO users VALUES (?,?,?)"
-  c.execute(query,n, z, plantCreator())
-  # wrap in a funtion
-
-conn.commit()
-conn.close()
+def enter_plant_info(plant):
+  c = conn.cursor()
+  data = (plant.name, plant.breed, plant.owner)
+  query = "INSERT INTO plant_info VALUES (?,?,?)"
+  c.execute(query, data)
+  conn.commit()
+  c.close()
